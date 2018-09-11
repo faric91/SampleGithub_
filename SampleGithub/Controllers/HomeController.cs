2250b291957064213e661bcaacd70a84de6c7ecc
faric91 <a.v.krypsky@gmail.com>
2018-09-11 11:05:20 +0300

    add index-action

--- SampleGithub/Controllers/HomeController.cs
+++ SampleGithub/Controllers/HomeController.cs
@@ -11,7 +11,6 @@ namespace SampleGithub.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            var sampleVar = new DateTime();
             return View();
         }
     }
